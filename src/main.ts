bdeea69267298c8dafbaa3f354a9034c1feb624c
Vlad Kondakov <vladisloove.kondakov@gmail.com>
2021-10-25 21:33:02 +0300

    init

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from "@nestjs/core"
+import { ValidationPipe } from "@nestjs/common"
+import Config from "config"
+
+import { AppModule } from "./app.module"
+
+async function bootstrap() {
+  const server: any = Config.get("server")
+
+  const app = await NestFactory.create(AppModule, {
+    logger: ["error", "log", "warn"],
+  })
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    })
+  )
+
+  app.setGlobalPrefix("api")
+  app.enableCors()
+
+  await app.listen(server.port)
+}
+bootstrap()
